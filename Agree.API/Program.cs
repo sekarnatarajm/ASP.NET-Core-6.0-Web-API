36f53c82455e1bb9bf0396ea6d4bcd8ee0139ce7
Sekar <146414821+sekarnatarajm@users.noreply.github.com>
2023-10-05 18:17:14 +0530

    Initial commit
    First commit

---
+++ Agree.API/Program.cs
@@ -0,0 +1,33 @@
+using Agree.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+startup.Configure(app, builder.Environment);
+// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
---
+++ Agree.API/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace Agree.API
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();  
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
